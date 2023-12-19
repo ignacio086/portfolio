aa2d0accffac7b36f59f86e87982986c715e5765
ignacio086 <ignaarigomez@gmail.com>
2023-12-18 21:11:46 -0300

    segundo commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,14 @@ export default function About(){
     return(
         <div id='sobremi'className="snap-center h-screen w-screen bg-white flex flex-row  items-center justify-center">
         <div className="w-1/2 h-full flex justify-center items-center">
-          <motion.div initial={{opacity:0,scale:0}} whileInView={{opacity:1,scale:1}} transition={{duration:1}} viewport={{once:true}} className="bg-gray-900 rounded-full w-1/2 h-1/2 flex items-center juistify-center text-center flex-row">
+          <motion.div initial={{opacity:0,scale:0}} whileInView={{opacity:1,scale:1}} transition={{duration:1}} viewport={{once:true}} className="bg-slate-800 rounded-full w-1/2 h-1/2 flex items-center juistify-center text-center flex-row">
             <div className="w-full">
               <h1 className="text-white text-center text-2xl">Sobre Mi</h1>
             </div>
           </motion.div>
         </div>
         <div className="w-1/2 h-full flex justify-center items-center">
-          <motion.div initial={{opacity:0,scale:0}} whileInView={{opacity:1,scale:1}} transition={{duration:2}} viewport={{once:true}}  className="bg-gray-900 rounded-full w-5/6 h-5/6 flex items-center juistify-center text-center flex-row">
+          <motion.div initial={{opacity:0,scale:0}} whileInView={{opacity:1,scale:1}} transition={{duration:2}} viewport={{once:true}}  className="bg-slate-800 rounded-full w-5/6 h-5/6 flex items-center juistify-center text-center flex-row">
             <div className="w-full p-24">
               <p className="text-white text-xs">Tengo 24 años y soy de Mendoza, Argentina. Empece a programar como hobbie despues de estudiar de forma autodidacta. Al final este pasatiempo paso a ser mi trabajo.</p>
             </div>
--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,53 +1,50 @@
 'use client'
 import Link from "next/link"
-
+import { FaCss3 , FaHtml5 , FaJs , FaReact , FaWordpress } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import { TbBrandNextjs , TbBrandMongodb } from "react-icons/tb";
 import { motion } from "framer-motion"
 export default function Experiencia(){
     return(
-        <div id='experiencia' className="snap-center h-screen w-screen bg-white flex flex-col pt-12">
-        <div className="w-screen h-1/5 flex flex-row">
-          <motion.div initial={{opacity:0,x:-200}} whileInView={{opacity:1,x:[-200,0]}} transition={{duration:2,delay:0}} viewport={{once:true}} className="w-1/2 h-full bg-gray-900 flex justify-center items-center">
-            <h1 className="text-white text-2xl">EXPERIENCIA</h1>
-          </motion.div>
-          <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2,delay:0}} viewport={{once:true}} className="w-1/2 h-full bg-white flex justify-center items-center">
-          </motion.div>
+      <div  id='experiencia' className="flex flex-row  items-center justify-center m-4 snap-center h-screen w-screen bg-white">
+        <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2}} viewport={{once:true}} className="h-screen w-screen bg-white grid grid-cols-3 grid-rows-3 gap-3 p-12">
+        <div className="flex flex-col justify-center items-center rounded-xl bg-slate-100 border-2 col-span-3 bg-gradient-to-t from-slate-600 to-slate-200">
+          <h1 className="text-3xl text-white">Experiencia</h1>
+          <p className="text-white">Trabajos realizados a lo largo de mi carrera como programador</p>
         </div>
-        <div className="w-screen h-1/5 flex flex-row">
-          <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2,delay:0.5}} viewport={{once:true}} className="w-1/2 h-full bg-white flex flex-col justify-center text-center items-center">
-            <p className="text-xs">Creado con NextJS y MongoDB. Una app realizada para crear,editar y borrar tareas</p>
-            <Link className="underline hover:underline-offset-4" href="https://github.com/ignacio086/Task-app">Conocer Mas</Link>
-          </motion.div>
-          <motion.div initial={{opacity:0,x:200}} whileInView={{opacity:1,x:[200,0]}} transition={{duration:2,delay:0.5}} viewport={{once:true}} className="w-1/2 h-full bg-gray-900 flex justify-center text-center items-center">
-            <h1 className="text-white text-2xl">App de Tareas</h1>
-          </motion.div>
+        <div className="rounded-xl bg-slate-100 border-2 row-span-2  bg-contain  bg-no-repeat bg-top" style={{backgroundImage:"url('fotografia.png')"}}>
+          <div className=" gap-4 p-8 text-center flex flex-col justify-end items-center rounded-xl w-full h-full" style={{background:'linear-gradient(0deg, rgba(15,23,42,1) 0%, rgba(15,23,42,0.8408613445378151) 62%, rgba(248,250,252,0) 100%)'}}>
+            <h1 className="text-3xl text-white">FOTOGRAFIAS</h1>
+            <p className="text-white">Pagina Web para Laura Becerra, fotografa de Mendoza.</p>
+            <div className="text-white flex gap-4 text-lg"><FaHtml5/><FaCss3/><FaJs/></div>
+            <Link className="text-white underline hover:underline-offset-4" href="https://github.com/ignacio086/Laura-fotografia">Conocer Mas</Link>
+          </div>
         </div>
-        <div className="w-screen h-1/5 flex flex-row">
-          <motion.div initial={{opacity:0,x:-200}} whileInView={{opacity:1,x:[-200,0]}} transition={{duration:2,delay:1}} viewport={{once:true}} className="w-1/2 h-full bg-gray-900 flex justify-center text-center items-center">
-            <h1 className="text-white text-2xl">Diseño Web</h1>
-          </motion.div>
-          <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2,delay:1}} viewport={{once:true}} className="p-4 w-1/2 h-full bg-white flex flex-col justify-center text-center items-center">
-            <p className="text-xs">Creado con HTML, CSS y JS. Pagina Web para Laura Becerra, fotografa de Mendoza.</p>
-            <Link className="underline hover:underline-offset-4" href="https://github.com/ignacio086/Laura-fotografia">Conocer Mas</Link>
-          </motion.div>
-        </div>
-        <div className="w-screen h-1/5 flex flex-row">
-          <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2,delay:1.5}} viewport={{once:true}} className="p-4 w-1/2 h-full bg-white flex flex-col justify-center text-center items-center">
-            <p className="text-xs">Creado con WordPress. Marketplace de productos sustentables de Mendoza.</p>
-            <Link className="underline hover:underline-offset-4" href="https://tribusustentable.com/">Conocer Mas</Link>
-          </motion.div>
-          <motion.div initial={{opacity:0,x:200}} whileInView={{x:[200,0],opacity:1}} transition={{duration:2,delay:1.5}} viewport={{once:true}} className="w-1/2 h-full bg-gray-900 flex justify-center items-center">
-            <h1 className="text-white text-2xl">TRIBÜ</h1>
-          </motion.div>
-        </div>
-        <div className="w-screen h-1/5 flex flex-row">
-          <motion.div initial={{opacity:0,x:-200}} whileInView={{opacity:1,x:[-200,0]}} transition={{duration:2,delay:0}} viewport={{once:true}} className="w-1/2 h-full bg-gray-900 flex justify-center items-center">
-            <h1 className="text-white text-2xl">Tragos</h1>
-          </motion.div>
-          <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:2,delay:1.5}} viewport={{once:true}} className="p-4 w-1/2 h-full bg-white flex flex-col justify-center text-center items-center">
-            <p className="text-xs">Creado con React, Axios y Redux. Muestra tarjetas de tragos consumidos a traves de una API</p>
+        <div className="rounded-xl bg-slate-100 border-2 bg-contain  bg-no-repeat bg-top" style={{backgroundImage:"url('tragos.png')"}}>
+          <div className="gap-2 p-4 text-white text-center flex flex-col justify-end items-center rounded-xl w-full h-full" style={{background:'linear-gradient(0deg, rgba(15,23,42,1) 0%, rgba(15,23,42,0.8408613445378151) 12%, rgba(248,250,252,0) 100%)'}}>
+            <h1 className="text-3xl ">TRAGOS</h1>
+            <p className="">Muestra tarjetas de tragos consumidos a traves de una API</p>
+            <div className=" flex gap-4 text-lg"><FaReact/><SiRedux/></div>
             <Link className="underline hover:underline-offset-4" href="https://github.com/ignacio086/Prueba-tecnica-ADEN">Conocer Mas</Link>
-          </motion.div>
+          </div>
         </div>
+        <div className="rounded-xl bg-slate-100 border-2 row-span-2 bg-contain  bg-no-repeat bg-top" style={{backgroundImage:"url('tribu.png')"}}>
+          <div className="gap-2 p-8  text-white text-center flex flex-col justify-end items-center rounded-xl w-full h-full" style={{background:'linear-gradient(0deg, rgba(15,23,42,1) 0%, rgba(15,23,42,0.8408613445378151) 12%, rgba(248,250,252,0) 100%)'}}>
+            <h1 className="text-3xl ">TRIBU</h1>
+            <p >Marketplace de productos sustentables de Mendoza.</p>
+            <div className=" flex gap-4 text-lg"><FaWordpress/></div>
+            <Link className="underline hover:underline-offset-4" href="https://tribusustentable.com/">Conocer Mas</Link>
+          </div>
+        </div>
+        <div className="rounded-xl bg-slate-100 border-2 bg-contain  bg-no-repeat bg-top" style={{backgroundImage:"url('tasks.png')"}}>
+          <div className="gap-2 p-4 text-white text-center flex flex-col justify-end items-center rounded-xl w-full h-full" style={{background:'linear-gradient(0deg, rgba(15,23,42,1) 0%, rgba(15,23,42,0.8408613445378151) 22%, rgba(248,250,252,0) 100%)'}}>
+            <h1 className="text-3xl ">TO DO</h1>
+            <p >Una app realizada para crear,editar y borrar tareas</p>
+            <div className=" flex gap-4 text-lg"><TbBrandNextjs/><TbBrandMongodb/></div>
+            <Link className="underline hover:underline-offset-4" href="https://github.com/ignacio086/Task-app">Conocer Mas</Link>
+          </div>
+        </div>
+        </motion.div>
       </div>
     )
 }
